7d95d8f342d96d575fe1c41215f2577ea1f84f85
untitledme <sayirezzzz@gmail.com>
2023-11-17 17:10:37 +0400

    done2

--- spheres.py
+++ spheres.py
@@ -4,15 +4,21 @@ import random
 
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 
 
 class Drawer(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
-        self.createSphere.clicked.connect(self.true)
+        self.resize(800, 600)
         self.flag = False
+        self.initUI()
+
+    def initUI(self):
+        self.createSphere = QPushButton("Создать окружность", self)
+        self.createSphere.resize(150, 51)
+        self.createSphere.move(320, 450)
+        self.createSphere.clicked.connect(self.true)
 
     def true(self):
         self.flag = True
@@ -32,14 +38,19 @@ class Drawer(QMainWindow):
         b = random.randint(100, 450)
         return a, b
 
+    @staticmethod
+    def generate() -> QColor:
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        return QColor(r, g, b)
+
     def draw(self, qp):
         x, y = self.coords()
         r = random.randint(10, 80)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(self.generate())
         qp.drawEllipse(x - r // 2, y - r // 2, r, r)
         self.flag = False
-
-
 
 
 if __name__ == '__main__':
